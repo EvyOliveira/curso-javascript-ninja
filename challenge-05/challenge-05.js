0c9630e57a5833965f2a9f462bd1560615f2d3af
Evelyn Cristini Oliveira <30937883+EvyOliveira@users.noreply.github.com>
2020-04-30 19:40:44 -0300

    Correção de testes da Lista 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,7 +3,7 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 
-var arr = [ 10, 'Evelyn', null, { a: 1 }, function() ];
+var arr = [10, 'Evelyn', null, { a: 1 }, function() {} ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
